9ea62fee3373dc33c9646f60e7e8f5fb61bdf35a
teoriyabomja <goringlass323@gmail.com>
2024-08-11 12:21:50 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from PyQt5 import QtWidgets
+from windows.main_window import Window
+
+import sys
+
+
+        
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = Window()
+    window.show()
+    app.exec_()
---
+++ windows/main_window.py
@@ -0,0 +1,7 @@
+from PyQt5.QtWidgets import QWidget
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Memory card')
