8ba6b6135da1723956f034b6bda5775df0f1220e
teoriyabomja <goringlass323@gmail.com>
2024-08-11 13:35:34 +0300

    add Ui for main_window

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from PyQt5 import QtWidgets
 from windows.main_window import Window
 
 import sys
-
-
-        
+    
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = Window()
